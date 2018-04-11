01ab0ec5c92d24a8c06a745f16bd2b2349290a6c
jw <cjw0515@naver.com>
2018-04-12 00:42:29 +0900

    no message

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser'; // PARSE HTML BODY
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+import mongoose from 'mongoose';
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/memoapp');
+
+app.use(bodyParser.json());
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
